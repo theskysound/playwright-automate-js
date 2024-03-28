02f482a6938651a40a6fcbf7abae904b87bd4584
theskysound <victorbalan20@gmail.com>
2024-03-28 02:08:44 -0700

    Fix add to cart button test

--- matchers/products.js
+++ matchers/products.js
@@ -1,9 +1,11 @@
-export const inventoryIitem = 'div.inventory_item'
+export const inventoryIitem = 'div.inventory_item';
 
-export const inventoryItemName = 'div.inventory_item_name'
+export const inventoryItemName = 'div.inventory_item_name';
 
-export const inventoryItemDesc = 'div.inventory_item_desc'
+export const inventoryItemDesc = 'div.inventory_item_desc';
 
-export const inventoryItemPrice = 'div.inventory_item_price'
+export const inventoryItemPrice = 'div.inventory_item_price';
 
-export const addToCartButton = (itemId) => `button#${itemId}`
+export const addToCartButton = (itemId) => `button#${itemId}`;
+
+export const inventoryItemButton = 'div.pricebar > button';
